ae7e0eae83ec1bc39d96e3db99f97742bdaccb2c
leninmhs <leninmhs@gmail.com>
2015-09-10 16:43:16 -0430

    ultimos cambios

--- inicio.pl
+++ inicio.pl
@@ -9,29 +9,33 @@ require 'config.pm';
 require 'operacionesdb.pl';
 
 use Text::CSV;
+use Pg::hstore;
 use Data::Dumper;
 use Switch;
 
 our $oradb;
 our $pgdb;
-$archivo = $ARGV[0] or die "Se requiere un archivo CSV como parametro\n";
 
 #my $csv = Text::CSV->new({ sep_char => ';' });
 my $csv = Text::CSV->new ( { sep_char => ';', binary => 1, auto_diag => 1, } )
                             or die "Cannot use CSV: ".Text::CSV->error_diag ();
 
-my $cont = -1;
+our $linea = 1;
+$archivo = $ARGV[0] or die "Se requiere un archivo CSV como parametro\n";
 our $id_desarrollo = $ARGV[1] or die "Se requiere el id_desarrollo como parametro\n";
 our $usuario_id_creacion = $ARGV[2] or die "Se requiere el usuario_id_creacion como parametro\n";
+our $id_carga_masiva = $ARGV[3] or die "Se requiere el usuario_id_creacion como parametro\n";
 our $id_unidad_multifamiliar = 0;
 our $id_vivienda = 0;
+mensajesCarga( "inicio", "inicio el proceso de la carga masiva N° $id_carga_masiva" );
 
 open( my $data, '<:encoding(utf8)', $archivo) or die "No puedo abrir el fichero '$archivo' $!\n";
 
 #$csv->getline ($data); #retiramos la cabecera
 $csv->column_names ($csv->getline ($data)); # use header
 while (my $campos = $csv->getline_hr( $data )) {
-  $cont = $cont + 1;
+  print $linea = $linea + 1;
+  #mensajesCarga( "info$cont", "inicio el recorrido del archivo de la carga masiva N° $id_carga_masiva" );
 
 #print Dumper($campos);
   $id_unidad_multifamiliar = unidadMultifamiliar( { unidad_multifamiliar =>
@@ -41,77 +45,12 @@ while (my $campos = $csv->getline_hr( $data )) {
                                     campos => $campos });
 
 
-### Consultamos cedula TABLAS_COMUNES.PERSONA en oracle
-  my $cedula = $campos->{cedula};
-  my $sth = $oradb->prepare("SELECT ID, NACIONALIDAD , CEDULA, PRIMER_NOMBRE AS PRIMERNOMBRE
-                          FROM TABLAS_COMUNES.PERSONA WHERE NACIONALIDAD = '1' AND CEDULA = $cedula ");
-  $sth->execute();
-  #my ($ID,$nacionalidad,$cedula, $primernombre)  = $sth->fetchrow_array();
-  @persona  = $sth->fetchrow_array();
-  $sth->finish();
-
-### Fin consulta TABLAS_COMUNES.PERSONA oracle
-
-my $ID = $persona[0];
-if( $ID eq ''){
-
-  $sth = $oradb->prepare("SELECT ID, NACIONALIDAD, CEDULA, PRIMERNOMBRE, SEGUNDONOMBRE, PRIMERAPELLIDO, SEGUNDOAPELLIDO, TO_DATE(FECHANACIMIENTO, 'DD-MM-YYYY' ) As FECHANACIMIENTO,2 AS PROCEDENCIA
-                          FROM ORGANISMOS_PUBLICOS.SAIME_ORIGINAL
-                          WHERE NACIONALIDAD ='V' AND CEDULA = $cedula ");
-  $sth->execute();
-  #my ($ID,$nacionalidad,$cedula, $primernombre)  = $sth->fetchrow_array();
-  @saime  = $sth->fetchrow_array();
-
-
-  #INSERT INTO TABLAS_COMUNES.PERSONA (ID, CEDULA,NACIONALIDAD,PRIMER_NOMBRE,SEGUNDO_NOMBRE,PRIMER_APELLIDO,SEGUNDO_APELLIDO,FECHA_NACIMIENTO,GEN_SEXO_ID,CODIGO_HAB,TELEFONO_HAB,CODIGO_MOVIL,TELEFONO_MOVIL,CORREO_PRINCIPAL)
-  #VALUES ((SELECT MAX(ID)+1 FROM TABLAS_COMUNES.PERSONA),17845965,1,'KARINA','LISBETH','NIEVES','PARRA',to_date('28-MAY-85','DD/MM/RR'),1,0212,6544565,0416,4565434,'karinitanieves@gmail.com')
-  ### Sacar ultimo ID en PERSONA
-  $sth = $oradb->prepare("SELECT MAX(ID)+1 FROM TABLAS_COMUNES.PERSONA");
-  $sth->execute();
-  #my ($ID,$nacionalidad,$cedula, $primernombre)  = $sth->fetchrow_array();
-  @id_persona  = $sth->fetchrow_array();
-
-  $sexo = ($campos->{sexo} eq 'MASCULINO') ? 2 : 1;
-
-  $persona_oracle = $oradb->prepare("INSERT INTO TABLAS_COMUNES.PERSONA(ID, CEDULA, NACIONALIDAD, PRIMER_NOMBRE, SEGUNDO_NOMBRE, PRIMER_APELLIDO, SEGUNDO_APELLIDO, FECHA_NACIMIENTO, GEN_SEXO_ID, CODIGO_HAB, TELEFONO_HAB, CODIGO_MOVIL, TELEFONO_MOVIL, CORREO_PRINCIPAL )
-         VALUES (?,?, ?, ?, ?, ?, ?, TO_DATE(?,'DD/MM/RR'), ?, ?, ?, ?, ?, ? )");
-  #my @persona = Funciones::picapersona( $consulta[8] );
-  #$persona_oracle->execute( 9625808,17845965,1,'KARINA','LISBETH','NIEVES','PARRA',TO_DATE('27/05/85','DD/MM/RR'),1,'0212','6544565','0416','4565434','karinitanieves@gmail.com');
-  $persona_oracle->execute( $id_persona[0],"$saime[2]",1,"$saime[3]","$saime[4]","$saime[5]","$saime[6]","$saime[7]",$sexo,'0212','6544565','0416','4565434', "$campos->{correo_electronico}");
-  my $id_oracle = $oradb->last_insert_id("null", "TABLAS_COMUNES", PERSONA, ID);
-  print "ULTIMO ID ORACLE: ".$id_persona[0]."\n";
-  #@persona  = $sth->fetchrow_array();
-
-  $persona_oracle->finish();
-  $persona_id = $id_persona[0];
-}else{
-  $persona_id = $persona[0];
-}
-
-if( defined( $persona_id ) && $persona_id ne '' ){
-
-print "PRINT ID PERSONA TABLAS_COMUNES: ".Dumper($persona_id );
-
-  $campos->{nacionalidad} = 97;
-
-
-  $nombre_completo = $campos->{primer_nombre}." ".$campos->{segundo_nombre}." ".$campos->{primer_apellido}." ".$campos->{segundo_apellido};
-
-  my $beneficiariotemp = $pgdb->prepare("INSERT INTO beneficiario_temporal(persona_id, desarrollo_id, unidad_habitacional_id, vivienda_id, nacionalidad, cedula, nombre_completo, estatus, usuario_id_creacion )
-         VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ? )");
-  #my @persona = Funciones::picapersona( $consulta[8] );
-  $beneficiariotemp->execute( $persona_id , $id_desarrollo, $id_unidad_multifamiliar, $id_vivienda, $campos->{nacionalidad}, $campos->{cedula},$nombre_completo,79, $usuario_id_creacion);
-  $id_beneficiario_temporal = $pgdb->last_insert_id("null", "public", beneficiario_temporal, id_beneficiario_temporal);
-
-
-}
+  $id_beneficiario_temporal = beneficiarioTemporal( { id_unidad_multifamiliar => $id_unidad_multifamiliar,
+                                    id_vivienda => $id_vivienda, campos => $campos });
 
   print "\nMultifamiliar: ".$id_unidad_multifamiliar." Vivienda:".$id_vivienda." Cedula: ".$campos->{cedula}." ID Oracle: ".$persona[0]." Cedula Oracle: ".$persona[2]." id_beneficiario_temporal: $id_beneficiario_temporal \n";
 
-}
-
-### Fin recorrido del CSV
-
+} ### Fin recorrido del CSV
 
 if (not $csv->eof) {
   $csv->error_diag();
@@ -119,4 +58,10 @@ if (not $csv->eof) {
 
 close $data;
 
-print "fin recorrido del CSV Cont: $cont\n";
+
+
+print "fin recorrido del CSV Cont: $linea\n";
+mensajesCarga( "info", "finalizo la carga masiva $id_carga_masiva" );
+
+#$pgdb->commit;  # or call $dbh->rollback; to undo changes
+#$pgdb->disconnect;
--- operacionesdb.pl
+++ operacionesdb.pl
@@ -29,7 +29,7 @@ sub unidadMultifamiliar($) {
       }
 
         return $id_unidad_multifamiliar;
-        
+
 }### fin unidad multifamiliar
 
 
@@ -49,9 +49,9 @@ sub unidadFamiliar($) {
       #$args->{campos}{area_mt2}
       #$args->{campos}{numero_de_piso}
       #$args->{campos}{numero_de_vivienda}
-      $args->{campos}{sala}      = TRUE;
-      $args->{campos}{comedor}   = TRUE;
-      $args->{campos}{lavandero} = TRUE;
+      #$args->{campos}{sala}      = TRUE;
+      #$args->{campos}{comedor}   = TRUE;
+      #$args->{campos}{lavandero} = TRUE;
       #$args->{campos}{lindero_norte_vivienda}
       #$args->{campos}{lindero_sur_vivienda}
       #$args->{campos}{lindero_este_vivienda}
@@ -64,7 +64,23 @@ sub unidadFamiliar($) {
       #$args->{campos}{numero_de_banos}
       #91
       #75
-      $args->{campos}{cocina}  = TRUE;
+      #$args->{campos}{cocina}  = TRUE;
+      #$args->{campos}{sala}      = TRUE;
+      #$args->{campos}{comedor}   = TRUE;
+      #$args->{campos}{lavandero} = TRUE;
+
+      $sala = ($args->{campos}{sala} eq 'SI') ? TRUE : ( ($args->{campos}{sala} eq 'NO') ? FALSE
+                      : mensajesCarga( "sala-$linea", "Valor para sala incorrecto en linea $linea" ));
+
+      $comedor = ($args->{campos}{comedor} eq 'SI') ? TRUE : ( ($args->{campos}{comedor} eq 'NO') ? FALSE
+                      : mensajesCarga( "comedor-$linea", "Valor para comedor incorrecto en linea $linea" ));
+
+      $lavandero = ($args->{campos}{lavandero} eq 'SI') ? TRUE : ( ($args->{campos}{lavandero} eq 'NO') ? FALSE
+                      : mensajesCarga( "lavandero-$linea", "Valor para lavandero incorrecto en linea $linea" ));
+
+      $cocina = ($args->{campos}{cocina} eq 'SI') ? TRUE : ( ($args->{campos}{cocina} eq 'NO') ? FALSE
+                      : mensajesCarga( "cocina-$linea", "Valor para cocina incorrecto en linea $linea" ));
+
       #$usuario_id_creacion
 
 
@@ -75,7 +91,7 @@ sub unidadFamiliar($) {
             $multifamiliar = $pgdb->prepare("INSERT INTO vivienda(tipo_vivienda_id,unidad_habitacional_id,construccion_mt2,nro_piso, nro_vivienda, sala, comedor,lavandero,lindero_norte,lindero_sur,lindero_este,lindero_oeste,coordenadas,precio_vivienda,nro_estacionamientos,descripcion_estac,nro_habitaciones,nro_banos,fuente_datos_entrada_id,estatus_vivienda_id,cocina,usuario_id_creacion )
                    VALUES ( ?, ?, ?, ?, ?, ?, ?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,? )");
             #my @persona = Funciones::picapersona( $consulta[8] );
-            $multifamiliar->execute( 94, $args->{id_unidad_multifamiliar}, $args->{campos}{area_mt2}, $args->{campos}{numero_de_piso}, $args->{campos}{numero_de_vivienda},$args->{campos}{sala},$args->{campos}{comedor}, $args->{campos}{lavandero},$args->{campos}{lindero_norte_vivienda},$args->{campos}{lindero_sur_vivienda},$args->{campos}{lindero_este_vivienda},$args->{campos}{lindero_oeste_vivienda},$args->{campos}{coordenadas}, $args->{campos}{precio_de_vivienda},$args->{campos}{puesto_estacionamiento},$args->{campos}{numero_estacionamiento},$args->{campos}{numero_de_habitaciones},$args->{campos}{numero_de_banos},91,75,$args->{campos}{cocina}, $usuario_id_creacion);
+            $multifamiliar->execute( 94, $args->{id_unidad_multifamiliar}, $args->{campos}{area_mt2}, $args->{campos}{numero_de_piso}, $args->{campos}{numero_de_vivienda}, $sala, $comedor, $lavandero,$args->{campos}{lindero_norte_vivienda},$args->{campos}{lindero_sur_vivienda},$args->{campos}{lindero_este_vivienda},$args->{campos}{lindero_oeste_vivienda},$args->{campos}{coordenadas}, $args->{campos}{precio_de_vivienda},$args->{campos}{puesto_estacionamiento},$args->{campos}{numero_estacionamiento},$args->{campos}{numero_de_habitaciones},$args->{campos}{numero_de_banos},91,75, $cocina, $usuario_id_creacion);
             $id_vivienda = $pgdb->last_insert_id("null", "public", vivienda, id_vivienda);
           }
           else {@unidad_familiar = $sth->fetchrow_array();
@@ -83,14 +99,105 @@ sub unidadFamiliar($) {
           }
       }
 
-        my ($number) = @_;
-        #return ($number + 10);
         return $id_vivienda;
 
 }### fin unidad multifamiliar
 
 
+sub beneficiarioTemporal($) {
+      my ($args) = @_;
+
+      ### Consultamos cedula TABLAS_COMUNES.PERSONA en oracle
+        my $cedula = $args->{campos}{cedula};
+        $nacionalidad = ($args->{campos}{nacionalidad} eq 'V') ? 1 : ( ($args->{campos}{nacionalidad} eq 'E') ? 0
+                        : mensajesCarga( "nacionalidad-$linea", "nacionalidad incorrecta en linea $linea" ));
+
+        my $sth = $oradb->prepare("SELECT ID, NACIONALIDAD , CEDULA, PRIMER_NOMBRE AS PRIMERNOMBRE
+                                FROM TABLAS_COMUNES.PERSONA WHERE NACIONALIDAD = '$nacionalidad' AND CEDULA = $cedula ");
+        $sth->execute();
+        #my ($ID,$nacionalidad,$cedula, $primernombre)  = $sth->fetchrow_array();
+        @persona  = $sth->fetchrow_array();
+        $sth->finish();
+
+      ### Fin consulta TABLAS_COMUNES.PERSONA oracle
+
+      my $ID = $persona[0];
+      if( $ID eq ''){
+
+        $sth = $oradb->prepare("SELECT ID, NACIONALIDAD, CEDULA, PRIMERNOMBRE, SEGUNDONOMBRE, PRIMERAPELLIDO, SEGUNDOAPELLIDO, TO_DATE(FECHANACIMIENTO, 'DD-MM-YYYY' ) As FECHANACIMIENTO,2 AS PROCEDENCIA
+                                FROM ORGANISMOS_PUBLICOS.SAIME_ORIGINAL
+                                WHERE NACIONALIDAD ='$args->{campos}{nacionalidad}' AND CEDULA = $cedula ");
+        $sth->execute();
+        #my ($ID,$nacionalidad,$cedula, $primernombre)  = $sth->fetchrow_array();
+        @saime  = $sth->fetchrow_array();
 
 
+        #INSERT INTO TABLAS_COMUNES.PERSONA (ID, CEDULA,NACIONALIDAD,PRIMER_NOMBRE,SEGUNDO_NOMBRE,PRIMER_APELLIDO,SEGUNDO_APELLIDO,FECHA_NACIMIENTO,GEN_SEXO_ID,CODIGO_HAB,TELEFONO_HAB,CODIGO_MOVIL,TELEFONO_MOVIL,CORREO_PRINCIPAL)
+        #VALUES ((SELECT MAX(ID)+1 FROM TABLAS_COMUNES.PERSONA),17845965,1,'KARINA','LISBETH','NIEVES','PARRA',to_date('28-MAY-85','DD/MM/RR'),1,0212,6544565,0416,4565434,'karinitanieves@gmail.com')
+        ### Sacar ultimo ID en PERSONA
+        $sth = $oradb->prepare("SELECT MAX(ID)+1 FROM TABLAS_COMUNES.PERSONA");
+        $sth->execute();
+        #my ($ID,$nacionalidad,$cedula, $primernombre)  = $sth->fetchrow_array();
+        @id_persona  = $sth->fetchrow_array();
+
+        $sexo = ($args->{campos}{sexo} eq 'MASCULINO') ? 2 : 1;
+
+        $persona_oracle = $oradb->prepare("INSERT INTO TABLAS_COMUNES.PERSONA(ID, CEDULA, NACIONALIDAD, PRIMER_NOMBRE, SEGUNDO_NOMBRE, PRIMER_APELLIDO, SEGUNDO_APELLIDO, FECHA_NACIMIENTO, GEN_SEXO_ID, CODIGO_HAB, TELEFONO_HAB, CODIGO_MOVIL, TELEFONO_MOVIL, CORREO_PRINCIPAL )
+               VALUES (?,?, ?, ?, ?, ?, ?, TO_DATE(?,'DD/MM/RR'), ?, ?, ?, ?, ?, ? )");
+        #my @persona = Funciones::picapersona( $consulta[8] );
+        #$persona_oracle->execute( 9625808,17845965,1,'KARINA','LISBETH','NIEVES','PARRA',TO_DATE('27/05/85','DD/MM/RR'),1,'0212','6544565','0416','4565434','karinitanieves@gmail.com');
+        $persona_oracle->execute( $id_persona[0],"$saime[2]",$nacionalidad,"$saime[3]","$saime[4]","$saime[5]","$saime[6]","$saime[7]",$sexo,'0212','6544565','0416','4565434', "$args->{campos}{correo_electronico}");
+        my $id_oracle = $oradb->last_insert_id("null", "TABLAS_COMUNES", PERSONA, ID);
+        print "ULTIMO ID ORACLE: ".$id_persona[0]."\n";
+        #@persona  = $sth->fetchrow_array();
+
+        $persona_oracle->finish();
+        $persona_id = $id_persona[0];
+      }else{
+        $persona_id = $persona[0];
+      }
+
+      if( defined( $persona_id ) && $persona_id ne '' ){
+
+      print "PRINT ID PERSONA TABLAS_COMUNES: ".Dumper($persona_id );
+
+      $nacionalidad = ($args->{campos}{nacionalidad} eq 'V') ? 97 : ( ($args->{campos}{nacionalidad} eq 'E') ? 98
+                      : mensajesCarga( "nacionalidad-$linea", "nacionalidad incorrecta en linea $linea" ));
+
+      $nombre_completo = $args->{campos}{primer_nombre}." ".$args->{campos}{segundo_nombre}." ".$args->{campos}{primer_apellido}." ".$args->{campos}{segundo_apellido};
+
+      my $beneficiariotemp = $pgdb->prepare("INSERT INTO beneficiario_temporal(persona_id, desarrollo_id, unidad_habitacional_id, vivienda_id, nacionalidad, cedula, nombre_completo, estatus, usuario_id_creacion, carga_masiva_id )
+               VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ? )");
+        #my @persona = Funciones::picapersona( $consulta[8] );
+        $beneficiariotemp->execute( $persona_id , $id_desarrollo, $id_unidad_multifamiliar, $id_vivienda, $nacionalidad, $args->{campos}{cedula},$nombre_completo,79, $usuario_id_creacion, $id_carga_masiva);
+        $id_beneficiario_temporal = $pgdb->last_insert_id("null", "public", beneficiario_temporal, id_beneficiario_temporal);
+
+
+      }
+
+      return $id_beneficiario_temporal;
+
+}### fin beneficiario temporal
+
+sub mensajesCarga {
+  my ($clave, $valor) = @_;
+
+  $addinfo->{$clave} = $valor;
+
+#UPDATE carga_masiva SET mensajes_carga = mensajes_carga || hstore('miclave','mivalor') where id_carga_masiva=73
+
+
+#  $pgdb->do("UPDATE carga_masiva SET mensajes_carga = mensajes_carga || ? where id_carga_masiva=?", undef,
+#     Pg::hstore::encode($addinfo), $id_carga_masiva);
+
+  #$pgdb->do("UPDATE carga_masiva SET mensajes_carga = mensajes_carga || ? where id_carga_masiva=?", undef,
+  #            Pg::hstore::encode($addinfo), $id_carga_masiva);
+          #    $pgdb->commit or die ;
+
+  my $update_carga_masiva = $pgdb->prepare("UPDATE carga_masiva SET mensajes_carga = mensajes_carga || ? where id_carga_masiva=?");
+   $update_carga_masiva->execute( Pg::hstore::encode($addinfo), $id_carga_masiva );
+  # $pgdb->commit;
+print "por aqui paso";
+}### fin mensajes Carga
 
 1;
